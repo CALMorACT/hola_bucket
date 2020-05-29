f1ae9a5797d844588299d665b9068b7d38c84760
holakk <2646677495@qq.com>
2020-05-29 17:10:32 +0800

    feat(control): add the search for v2ray-rule-dat

---
+++ test/v2ray-rules-search.ps1
@@ -0,0 +1,50 @@
+param($type = "all", $link)
+
+$direct = "direct-list"
+$proxy = "proxy-list"
+$reject = "reject-list"
+
+
+
+function get_info([int]$typenum, [string]$link) {
+    $address
+    if ($typenum -eq 1)
+    {
+        $address = $direct
+    }
+    elseif ($typenum -eq 2)
+    {
+        $address = $proxy
+    }
+    elseif ($typenum -eq 3)
+    {
+        $address = $reject
+    }
+    elseif ($typenum -eq 4)
+    {
+        $address = "*-list"
+    }
+    Select-String -Pattern "$link" -Path "$PSScriptRoot/$address"
+}
+
+if (!($link)){
+    "Usage: v2rulesearch <type> <link>`n`nSome useful types are:`n`n"
+    "direct or d: only search direct link"
+    "proxy or p: only search proxy link"
+    "reject or r: only search reject link"
+    "all or a: search all types of link, the dafult option"
+}
+else {
+    if ($type -eq "direct" || $type -eq "d") {
+        get_info 1 $link
+    }
+    elseif ($type -eq "proxy" || $type -eq "p") {
+        get_info 2 $link
+    }
+    elseif ($type -eq "reject" || $type -eq "r"){
+        get_info 3 $link
+    }
+    elseif ($type -eq "all" || $type -eq "a"){
+        get_info 4 $link
+    }
+}
