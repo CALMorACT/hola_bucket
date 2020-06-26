f62ed0a37166468898ad61d1b6197fea2b7e710a
holakk <2646677495@qq.com>
2020-06-26 14:01:24 +0800

    feat(data): add gig suport

---
+++ test/gig.ps1
@@ -0,0 +1,15 @@
+#For PowerShell v3
+param(
+    [Parameter(Mandatory = $true)]
+    [string[]]$list
+)
+
+Function gig {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]$list
+    )
+    $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -path $pwd -ChildPath ".gitignore") -Encoding ascii;
+}
+gig $list
